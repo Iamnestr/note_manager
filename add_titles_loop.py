e4750f728f06a397e79d763d160c780fe1941b38
Денис Н. <nestrforce@gmail.com>
2024-12-13 17:29:15 +0300

    new_1

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -13,3 +13,18 @@ print('\nЗаголовки заметки:')
 for i in range(range_):
     print("-", titles[i])
 
+# Второй вариант
+# Создаем список заголовков заметки
+titles = []
+range_ = -1
+title = None
+while title != "":
+    title = str(input('Введите заголовок заметки (или оставьте пустым для завершения): '))
+    range_ += 1
+    titles.append(title)
+titles.remove("")
+
+# Выводим все заголовки
+print('\nЗаголовки заметки:')
+for i in range(range_):
+    print("-", titles[i])
