eff4b6058688f8fa99dd46b68478593ac4abda43
Денис Н. <nestrforce@gmail.com>
2024-12-21 23:19:29 +0300

    new_7

--- search_notes_function.py
+++ search_notes_function.py
@@ -2,23 +2,29 @@
 def search_notes(notes, keyword=None, status=None):
     notes_found = []
 
+    # Функция для сбора значений заметки 'username', 'title', 'content' в одну строку
+    def values_all():
+        values = ''
+        for v in note.values():
+            values += v.lower()
+            if values == 'created_date':
+                break
+        return values.find(keyword.lower())
+
     # Проверяем какой критерий для поиска задан, потом ищем и собираем их в один лист
     if keyword is not None and status is not None:
         for i in range(len(notes)):
             note = notes[i]
-            search_keyword = note['username'].lower() + " " + note['title'].lower() + " " + note['content'].lower()
-            if (keyword.lower() in search_keyword.split()
-                    and status.lower() == note['status'].lower()):
+            if values_all() >= 0 and status.lower() == note['status'].lower():
                 notes_found.append(note)
     elif keyword is not None:
-        for i in range(len(notes)):
-            note = notes[i]
-            search_keyword =  note['username'].lower() + " " + note['title'].lower() + " " + note['content'].lower()
-            if keyword.lower() in search_keyword.split():
+        for j in range(len(notes)):
+            note = notes[j]
+            if values_all() >= 0:
                 notes_found.append(note)
     elif  status is not None:
-        for i in range(len(notes)):
-            note = notes[i]
+        for l in range(len(notes)):
+            note = notes[l]
             if status.lower() == note['status'].lower():
                 notes_found.append(note)
     return notes_found
@@ -29,18 +35,19 @@ def display_notes(notes_):
         print("Заметки, соответствующие запросу, не найдены.")
     else:
         print("\nНайденные заметки:")
-        for i in range(len(notes_)):
-            print(f"Заметка № {i+1}:")
-            note = notes_[i]
+        for s in range(len(notes_)):
+            print(f"Заметка № {s+1}:")
+            note = notes_[s]
             keys = ["Имя пользователя", "Заголовок", "Описание", "Статус", "Дата создания", "Дата дедлайна"]
             keys_index = 0
             for value in note.values():
                 print(f"{keys[keys_index]}: {value}")
                 keys_index += 1
-            print("-----------------")
+                if keys_index == 4:
+                    break
 
 # Создание списка заметок, с которым будем работать
-notes = [{'username': 'Алексей', 'title': 'Список покупок', 'content': 'Купить продукты на неделю',
+notes_all = [{'username': 'Алексей', 'title': 'Список покупок', 'content': 'Купить продукты на неделю',
           'status': 'новая', 'created_date': '27-11-2024', 'issue_date': '30-11-2024'},
          {'username': 'Мария', 'title': 'Учеба', 'content': 'Подготовиться к экзамену',
           'status': 'в процессе', 'created_date': '25-11-2024', 'issue_date': '01-12-2024'},
@@ -49,12 +56,12 @@ notes = [{'username': 'Алексей', 'title': 'Список покупок', 
 
 # Отображение исходного списка
 print(f"Исходный список заметок:\nnotes =", end= " ")
-if not notes:
+if not notes_all:
     print("[]")
 else:
     print("[")
-    for i in range(len(notes)):
-        print(notes[i])
+    for t in range(len(notes_all)):
+        print(notes_all[t])
     print("]")
 
 # Выбор метода поиска
@@ -75,6 +82,6 @@ elif search_method == 3:
 else:
     print("Не выбран метод поиска")
 
-notes_total = search_notes(notes, keyword=keyword_, status=status_)
+notes_total = search_notes(notes_all, keyword_, status_)
 
 display_notes(notes_total)
