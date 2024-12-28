fb1c82984b4332cb30675c679553503f6fa8a3c1
Денис Н. <nestrforce@gmail.com>
2024-12-29 00:24:01 +0300

    finish

---
+++ load_notes_from_file.py
@@ -0,0 +1,16 @@
+# Файловый вывод
+import yaml
+
+# Функция для выгрузки заметок из файла
+def save_notes_to_file():
+    with open('filename.txt', 'r', encoding='utf-8') as file:
+        notes = yaml.safe_load(file.read())
+
+    # Смена ключей в словарях
+    for note_keys in range(len(notes)):
+        list_keys = ["username", "title", "content", "status", "created_date", "issue_date"]
+        notes[note_keys] = dict(zip(list_keys, list(notes[note_keys].values())))
+    print(notes)
+
+# Функция для выгрузки заметок из файла
+save_notes_to_file()
