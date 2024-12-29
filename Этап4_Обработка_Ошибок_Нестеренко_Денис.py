1501ad0b35e16fe1c304ddfd86c0e561e9a96c0e
Денис Н. <nestrforce@gmail.com>
2024-12-29 10:40:53 +0300

    finish

---
+++ Этап4_Обработка_Ошибок_Нестеренко_Денис.py
@@ -0,0 +1,15 @@
+filename = 'corrupted_file.txt'
+try:
+    with open(filename) as file:
+        print(file.read())
+except FileNotFoundError:
+    print(f'Файл {filename} не найден. Создан новый файл')
+    file = open(filename, 'x')
+except UnicodeDecodeError:
+    print(f'Не удалось декодировать файл {filename}')
+except PermissionError:
+    print('Ошибка доступа')
+except Exception as e:
+    print(f"Ошибка: {e}")
+finally:
+    print('Проверка ошибок проведена')
